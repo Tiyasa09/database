db176079354c15772514d539056668cae5ee5bee
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-11-25 15:16:19 +0530

    Add files via upload

---
+++ emp2.sql
@@ -0,0 +1,14 @@
+use c2268;
+create table emp2(empID numeric(6),last_name varchar(25),job_ID varchar(10),salaty integer(6),
+common_percen integer(4),manager_ID integer (6),dept_ID numeric(4));
+insert into emp2 values
+(1234,'naskar','IT',32000,34,5678,987),
+(2468,'sarkar','technical',23000,35,1678,234),
+(1235,'naskar','freshers',22000,36,2678,907),
+(1534,'mondal','manager',25000,30,3678,917),
+(1734,'pal','IT',30000,31,4678,927),
+(1239,'sarkar','IT',24000,32,6678,937),
+(3579,'roy','freshers',20000,38,7678,947),
+(1537,'mondal','manager',21000,40,8678,957),
+(2131,'pal','IT',32000,31,9678,967),
+(4057,'roy','technical',35000,341,5078,977);

25ea25196b73836f025321bc3a9d0b1b8c17fb46
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-11-30 14:13:49 +0530

    Add files via upload

---
+++ emp3.sql
@@ -0,0 +1,14 @@
+use c2268;
+create table emp3(emp_ID int primary key,empname varchar(20) not null,designation varchar(20) not null,
+dept varchar(20),working_hour int(10),salary integer(6));
+insert into emp3 values
+(1234,'tiyasa','manager','IT',9,40000),
+(2345,'puja','technical','IT',9,25000),
+(3456,'bidisha','manager','IT',9,40000),
+(4567,'sanjukta','developer','IT',9,30000),
+(5678,'dona','freshers','IT',9,20000),
+(6789,'nilu','manager','IT',9,40000),
+(7890,'tanu','developer','IT',9,30000),
+(1210,'anu','manager','IT',9,40000),
+(1276,'ankita','freshers','IT',9,20000),
+(9234,'srijita','freshers','IT',9,20000);
